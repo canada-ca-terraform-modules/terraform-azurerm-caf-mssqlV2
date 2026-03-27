23e287f7db45aa48e26adc50b50de304931fb42b
Bernard Maltais <bernard.maltais@ssc-spc.gc.ca>
2026-03-27 08:49:00 -0400

    chore: bump ESLZ module ref to v1.1.0

--- ESLZ/mssql.tf
+++ ESLZ/mssql.tf
@@ -9,7 +9,7 @@ variable "SQL_Server" {
 }
 
 module "mssql" {
-  source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-mssqlV2.git?ref=v1.0.6"
+  source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-mssqlV2.git?ref=v1.1.0"
   for_each = var.SQL_Server
 
   userDefinedString    = each.key
